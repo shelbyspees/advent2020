1aa5a46946bba8e1a8aacd97be569c7d0775d63b
Shelby Spees <shelbyspees@gmail.com>
2020-11-30 21:19:28 -0800

    Day 01 - part 1 naive solution

---
+++ day01.rb
@@ -0,0 +1,30 @@
+# Before you leave, the Elves in accounting just need you to fix your expense report (your puzzle input); apparently, something isn't quite adding up.
+# Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
+# For example, suppose your expense report contained the following:
+
+# 1721, 979, 366, 299, 675, 1456
+# In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
+
+# Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
+
+# get list of numbers from file
+numbers = []
+
+File.open('day01.txt', 'r') do |f|
+  f.each_line do |line|
+    numbers.push line.to_i
+  end
+end
+
+test_numbers = [1721, 979, 366, 299, 675, 1456]
+
+# O(n^2) solution
+numbers.each do |x|
+  numbers.each do |y|
+    unless y == x
+      if (x + y == 2020)
+        return x * y
+      end
+    end
+  end
+end
